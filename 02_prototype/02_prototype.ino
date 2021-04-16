c9791b7f413005cdf001bd4f98240632d72b7c71
XX <fallwall19@gmail.com>
2021-04-15 22:16:16 -0700

    started

---
+++ 02_prototype/02_prototype.ino
@@ -0,0 +1,16 @@
+#include <Keypad.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //four columns
+//define the cymbols on the buttons of the keypads
+char hexaKeys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+byte rowPins[ROWS] = {34,35,36,37}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {31,32,33}; //connect to the column pinouts of the keypad
+
+//initialize an instance of class NewKeypad
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
